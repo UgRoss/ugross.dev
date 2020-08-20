35f29432b2f1340c78cf513f7b79a993968097eb
Rostyslav Ugryniuk <ug.ross@hotmail.com>
2020-08-20 11:11:38 +0300

    :fire: remove deprecated HandheldFriendly meta tag

--- src/html.tsx
+++ src/html.tsx
@@ -17,9 +17,8 @@ const HTML: React.FC<HTMLProps> = ({ headComponents, body, postBodyComponents })
 
         {/* Mobile Meta */}
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="HandheldFriendly" content="True" />
 
-        {/* Styles'n'Scripts */}
+        {/* Styles and Scripts */}
         <link
           rel="stylesheet"
           type="text/css"
