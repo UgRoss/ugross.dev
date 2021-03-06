c18f2c934037a323a42e6685ce515ac466bca9da
Rostyslav Ugryniuk <ug.ross@hotmail.com>
2021-03-07 00:57:40 +0200

    Add separator to PostPreview component

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from '~/components/Link';
+
+const Article = styled.article.attrs(({ className = '' }) => ({
+  className: `pb-md ${className}`,
+}))`
+  position: relative;
+
+  & + & {
+    padding-top: var(--space-md, 1rem);
+    margin-top: var(--space-md, 1rem);
+
+    &:before {
+      content: '';
+      border-top: 1px solid var(--color-horizontal-rule, #ccc);
+      width: 10rem;
+      position: absolute;
+      top: 0;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+  }
+`;
 
 interface PostPreviewProps {
   url: string;
@@ -9,7 +31,7 @@ interface PostPreviewProps {
 }
 
 export const PostPreview: React.FC<PostPreviewProps> = ({ url, title, excerpt, className = '' }) => (
-  <article className={`pt-md pb-md ${className}`}>
+  <Article className={className}>
     <header>
       <h3>
         <Link to={url} asText={true}>
@@ -23,5 +45,5 @@ export const PostPreview: React.FC<PostPreviewProps> = ({ url, title, excerpt, c
     <footer>
       <Link to={url}>Read this article →</Link>
     </footer>
-  </article>
+  </Article>
 );
