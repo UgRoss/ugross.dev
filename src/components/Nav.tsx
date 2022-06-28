d8cce786d60e6d18221047a25b0826f0adade6bf
Rostyslav Ugryniuk <ug.ross@hotmail.com>
2022-06-28 22:36:57 +0300

    Make Navbar sticky

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CommandPaletteButton } from '~/components/CommandPaletteButton';
+import { useScroll } from '~/hooks/useScroll';
 import Link from 'next/link';
 
 interface NavProps {
@@ -7,16 +8,27 @@ interface NavProps {
 }
 
 export const Nav: React.FC<NavProps> = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const position = useScroll(200);
+
+  useEffect(() => {
+    setScrolled(scrolled ? position.y > 50 : position.y > 50);
+  }, [position]);
+
   return (
-    <nav>
-      <div className="container px-6 py-6 mx-auto">
+    <nav
+      className={`sticky top-0 z-30 w-full bg-body ${
+        scrolled ? 'border-b dark:border-gray-800' : ''
+      }`}
+    >
+      <div className="container mx-auto py-3 px-6">
         <div className="flex flex-row-reverse items-center justify-between">
           <div className="flex items-center justify-between">
             <div className="flex items-center space-x-1">
               <CommandPaletteButton />
             </div>
           </div>
-          <div className={`flex-1 flex items-center justify-between`}>
+          <div className="flex flex-1 items-center justify-between">
             <div className="flex flex-row items-center sm:mr-4">
               <span className="dark:text-white">
                 <Link href="/">
