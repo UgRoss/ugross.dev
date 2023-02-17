6e6a2d3b591d1b5ce5f70e8ba0ec763c5ba0a34d
Rostyslav Ugryniuk <ugr.ross@gmail.com>
2023-02-17 15:06:51 +0200

    clean up

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-/** Detect if the user has requested the system use a light or dark color theme */
-export const supportsDarkMode = (): boolean =>
-  window && window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-/** Transform object into query string */
-export const serialize = (data: Record<string, string>): string =>
-  Object.keys(data)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&');
