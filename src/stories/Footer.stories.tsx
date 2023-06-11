72fd82227dac9917cd1ff9112764edf105e3962c
Rostyslav Ugryniuk <ug.ross@hotmail.com>
2023-06-11 16:41:42 +0300

    Add Footer component and Storybook stories (#142)
    * Add Footer component and stories
    
    * Cleanup comments
    
    * Remove additional empty new line

---
+++ src/stories/Footer.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Footer } from '~/components/Footer';
+
+const defaultFooterLinks = [
+  { title: 'Colophon', href: '#' },
+  { title: 'Uses', href: '#' },
+  { title: 'Github', href: '#' },
+  { title: 'RSS', href: '#' },
+];
+
+const meta: Meta<typeof Footer> = {
+  title: 'Example/Footer',
+  component: Footer,
+  tags: [],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Footer>;
+
+export const Default: Story = {
+  args: {
+    links: defaultFooterLinks,
+  },
+};
+
+export const WithoutLinks: Story = {
+  args: {
+    links: [],
+  },
+};
