3d4dfb87180acf76011b73952030139711984339
Rostyslav Ugryniuk <ug.ross@hotmail.com>
2023-06-12 14:44:43 +0300

    Improve Link component, add icons lib

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,6 +1,29 @@
 import React, { ReactNode } from 'react';
 import NextLink, { LinkProps as NextLinkProps } from 'next/link';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { Icon } from '@phosphor-icons/react';
 import { cn, isExternalLink } from '~/utils';
+
+const linkVariants = cva('font-semibold flex items-center transition-all duration-300', {
+  variants: {
+    withUnderline: {
+      true: [
+        'no-underline relative',
+        'after:content-[""] after:absolute after:w-full after:h-0.5 after:-bottom-0.5 after:left-0 after:transition-all after:duration-300 after:rounded',
+        'after:bg-gray-300 dark:after:bg-gray-600 hover:cursor-pointer hover:dark:after:bg-primary hover:after:bg-primary',
+      ],
+      false: '',
+    },
+    textColor: {
+      default: 'dark:text-white',
+      primary: 'text-primary hover:text-white',
+    },
+  },
+  defaultVariants: {
+    withUnderline: true,
+    textColor: 'default',
+  },
+});
 
 type AnchorProps = React.DetailedHTMLProps<
   React.AnchorHTMLAttributes<HTMLAnchorElement>,
@@ -8,23 +31,42 @@ type AnchorProps = React.DetailedHTMLProps<
 >;
 
 type LinkProps = {
-  href: string;
   children: ReactNode;
   className?: string;
-} & (AnchorProps | NextLinkProps);
+  iconClassName?: string;
+  endIcon?: Icon;
+} & VariantProps<typeof linkVariants> &
+  (AnchorProps | NextLinkProps);
 
-export function Link(props: LinkProps) {
+export function Link({
+  withUnderline,
+  textColor,
+  endIcon: EndIcon,
+  className,
+  iconClassName,
+  children,
+  ...props
+}: LinkProps) {
   const shouldRenderAnchorElement = isExternalLink(props.href);
-  const linkClassName = cn(
-    'relative font-semibold dark:text-white no-underline',
-    'after:content-[""] after:absolute after:w-full after:h-0.5 after:-bottom-0.5 after:left-0 after:transition-all after:duration-300 after:rounded',
-    'after:bg-gray-300 dark:after:bg-gray-600 hover:cursor-pointer hover:dark:after:bg-primary hover:after:bg-primary',
-    props.className
+  const linkClassName = cn(linkVariants({ withUnderline, textColor, className }));
+  const content = (
+    <>
+      {children}
+      {EndIcon && <EndIcon className={cn('ml-1', iconClassName)} weight="bold" />}
+    </>
   );
 
   if (shouldRenderAnchorElement) {
-    return <a {...(props as AnchorProps)} className={linkClassName} />;
+    return (
+      <a {...(props as AnchorProps)} className={linkClassName}>
+        {content}
+      </a>
+    );
   }
 
-  return <NextLink {...(props as NextLinkProps)} className={linkClassName} />;
+  return (
+    <NextLink {...(props as NextLinkProps)} className={linkClassName}>
+      {content}
+    </NextLink>
+  );
 }
