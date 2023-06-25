c1ebef0b7aae4af451e8acb8954cc60221b73d31
Rostyslav Ugryniuk <ug.ross@hotmail.com>
2023-06-25 11:06:49 +0300

    Remove ring from Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -41,14 +41,14 @@ export function Input({
           onChange={onChange}
           type={type}
           id={id}
-          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 outline-0 focus:border-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500"
           placeholder={placeholder}
           required={required}
         />
         {showClearButton && (
           <button
             type="button"
-            className="absolute inset-y-0 right-0 flex items-center pr-3"
+            className="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-3"
             onClick={onClearButtonClick}
           >
             <X
