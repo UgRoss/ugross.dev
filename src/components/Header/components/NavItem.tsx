24f6ce55f0ac18d3d0a0887c4444cc2009e41414
Rostyslav Ugryniuk <ug.ross@hotmail.com>
2023-09-03 12:42:38 +0300

    add prop types to NavItem

--- src/components/Header/components/NavItem.tsx
+++ src/components/Header/components/NavItem.tsx
@@ -3,16 +3,21 @@ import { usePathname } from 'next/navigation';
 import { cn } from '~/utils';
 import { ActiveNavItemHighlighter } from './ActiveNavItemHighlighter';
 
-export function NavItem({ href, children }: any) {
+interface NavItemProps {
+  children: React.ReactNode;
+  href: string;
+}
+
+export function NavItem({ children, href }: NavItemProps) {
   const isActive = usePathname() === href;
 
   return (
     <Link
-      href={href}
       className={cn(
         'relative block px-3 py-2 font-medium transition',
         isActive ? 'text-primary' : 'hover:text-primary'
       )}
+      href={href}
     >
       {children}
       {isActive && <ActiveNavItemHighlighter />}
