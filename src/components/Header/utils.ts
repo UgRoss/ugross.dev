8f3e447c6ead71a72a7c4b9fd03a597d26a38e01
Rostyslav Ugryniuk <ug.ross@hotmail.com>
2023-09-04 20:06:07 +0300

    Update utils.ts

--- src/components/Header/utils.ts
+++ src/components/Header/utils.ts
@@ -1,4 +1,4 @@
-import { HeaderNavItem, HeaderNavItemDropdown } from './HeaderNavItem';
+import type { HeaderNavItem, HeaderNavItemDropdown } from './HeaderNavItem';
 
 export function isHeaderNavItemDropdown(item: HeaderNavItem): item is HeaderNavItemDropdown {
   return (item as HeaderNavItemDropdown).items !== undefined;
