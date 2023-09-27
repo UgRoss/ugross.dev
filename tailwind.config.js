0baccc4787c852dc89c992d5e341ec5bf0a2df82
Rostyslav Ugryniuk <ug.ross@hotmail.com>
2023-09-27 14:13:31 +0300

    Increase container width

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/stories/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: ['class'],
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+      },
+      screens: {
+        lg: '650px',
+        md: '650px',
+        sm: '100%',
+        xl: '650px',
+      },
+    },
     extend: {
+      backgroundColor: {
+        body: 'hsl(var(--body-background))',
+        'inline-code': 'var(--color-inline-code-background)',
+      },
       colors: {
-        heading: 'var(--color-heading)',
-        text: 'var(--color-text)',
+        accent: {
+          DEFAULT: 'hsl(var(--color-accent))',
+          foreground: 'hsl(var(--color-accent-foreground))',
+        },
         border: 'hsl(var(--color-border))',
-        muted: 'hsl(var(--color-text-muted) / <alpha-value>)',
-        'card-foreground': 'hsl(var(--color-card-foreground) / <alpha-value>)',
+        callout: {
+          DEFAULT: 'hsl(var(--color-callout-background))',
+        },
         card: {
           DEFAULT: 'hsl(var(--color-card-background))',
           foreground: 'hsl(var(--color-card-foreground))',
         },
+        'card-foreground': 'hsl(var(--color-card-foreground) / <alpha-value>)',
+        heading: 'var(--color-heading)',
+        muted: 'hsl(var(--color-text-muted) / <alpha-value>)',
         popover: {
           DEFAULT: 'hsl(var(--color-popover-background))',
           foreground: 'hsl(var(--color-popover-foreground))',
@@ -26,50 +49,27 @@ module.exports = {
         primary: {
           DEFAULT: 'hsl(var(--color-primary))',
         },
-        callout: {
-          DEFAULT: 'hsl(var(--color-callout-background))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--color-accent))',
-          foreground: 'hsl(var(--color-accent-foreground))',
-        },
+        text: 'var(--color-text)',
       },
       typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            code: false,
+            'code::after': false,
+            'code::before': false,
+            pre: false,
+            'pre code': false,
+          },
+        },
         page: {
           css: {
-            '--tw-prose-headings': theme('colors.heading'),
             '--tw-prose-body': theme('colors.text'),
+            '--tw-prose-headings': theme('colors.heading'),
             '--tw-prose-invert-body': theme('colors.text'),
             '--tw-prose-invert-headings': theme('colors.heading'),
           },
         },
-        DEFAULT: {
-          css: {
-            pre: false,
-            code: false,
-            'pre code': false,
-            'code::before': false,
-            'code::after': false,
-          },
-        },
       }),
-      backgroundColor: {
-        body: 'hsl(var(--body-background))',
-        'inline-code': 'var(--color-inline-code-background)',
-      },
-    },
-    container: {
-      center: true,
-      screens: {
-        sm: '100%',
-        md: '600px',
-        lg: '600px',
-        xl: '600px',
-      },
-      padding: {
-        DEFAULT: '1rem',
-      },
     },
   },
-  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
 };
