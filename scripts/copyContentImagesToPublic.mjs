973cd573492f7427306356c14f2bccdc79aa4266
Rostyslav Ugryniuk <ug.ross@hotmail.com>
2023-10-22 12:25:10 +0300

    Update copyContentImagesToPublic.mjs

--- scripts/copyContentImagesToPublic.mjs
+++ scripts/copyContentImagesToPublic.mjs
@@ -1,10 +1,12 @@
-import { emptyDir, copy } from 'fs-extra';
+import { emptyDir, copy, exists } from 'fs-extra';
 import path from 'path';
 
 const imagesFolderName = 'content-images';
 const imagesDir = path.resolve(process.cwd(), 'content', imagesFolderName);
 const targetDir = path.resolve(process.cwd(), 'public', imagesFolderName);
 
-emptyDir(path.resolve(targetDir));
+if (exists(targetDir)) {
+  await emptyDir(path.resolve(targetDir));
+}
 
 await copy(imagesDir, targetDir);
