414ed3ea3fe89052e0b89e4fe4cb889d72a49f2b
Rostyslav Ugryniuk <ug.ross@hotmail.com>
2023-10-23 12:05:44 +0300

    remove top margin from TILsList

--- src/app/til/components/TILsList.tsx
+++ src/app/til/components/TILsList.tsx
@@ -11,7 +11,7 @@ export function TILsList({ emptyMessage = messages.til.noItems, items }: Readonl
   const isEmpty = items.length === 0;
 
   return (
-    <div className="mt-10 flex flex-col gap-5">
+    <div className="flex flex-col gap-5">
       <ul className="animated-list">
         {isEmpty && <p className="text-center text-muted">{emptyMessage}</p>}
         {items.map(({ slug, tags, title }) => (
