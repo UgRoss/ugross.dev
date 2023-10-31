6ef58d05040c8e2968941125d74af0cbb53e237e
Rostyslav Ugryniuk <ug.ross@hotmail.com>
2023-10-31 13:13:17 +0200

    update manifest file to remove code duplications

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,6 +1,14 @@
 import { MetadataRoute } from 'next';
 import { DEFAULT_META } from '~/config';
 
+const maskableIconSizes = [512, 384, 192, 128, 96, 72];
+const maskableIcons: MetadataRoute.Manifest['icons'] = maskableIconSizes.map((size) => ({
+  purpose: 'any',
+  sizes: `${size}x${size}`,
+  src: `/pwa/maskable_icon_x${size}.png`,
+  type: 'image/png',
+}));
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
     background_color: '#222',
@@ -13,42 +21,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/pwa/pwa-icon.png',
         type: 'image/png',
       },
-      {
-        purpose: 'any',
-        sizes: '512x512',
-        src: '/pwa/maskable_icon_x512.png',
-        type: 'image/png',
-      },
-      {
-        purpose: 'any',
-        sizes: '384x384',
-        src: '/pwa/maskable_icon_x384.png',
-        type: 'image/png',
-      },
-      {
-        purpose: 'any',
-        sizes: '192x192',
-        src: 'static/pwa/maskable_icon_x192.png',
-        type: 'image/png',
-      },
-      {
-        purpose: 'any',
-        sizes: '128x128',
-        src: '/pwa/maskable_icon_x128.png',
-        type: 'image/png',
-      },
-      {
-        purpose: 'any',
-        sizes: '96x96',
-        src: '/pwa/maskable_icon_x96.png',
-        type: 'image/png',
-      },
-      {
-        purpose: 'any',
-        sizes: '72x72',
-        src: '/pwa/maskable_icon_x72.png',
-        type: 'image/png',
-      },
+      ...(maskableIcons || []),
     ],
     lang: 'en-US',
     name: 'Ross Blog',
